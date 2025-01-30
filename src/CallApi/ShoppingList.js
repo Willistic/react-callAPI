76a3d9ba3e806f071b556dd4a485787a63a2d8e2
Willistic <jlubbick@aol.com>
2025-01-30 14:59:04 +0100

    Refactor ShoppingList component for cleaner code and improved readability

--- src/CallApi/ShoppingList.js
+++ src/CallApi/ShoppingList.js
@@ -1,4 +1,3 @@
-import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -6,18 +5,11 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 
-const useStyles = makeStyles({
-	table: {
-		minWidth: 650,
-	},
-});
-
 export default function ShoppingList({ fetchedProduct }) {
-	const classes = useStyles();
 
 	return (
 		<TableContainer>
-			<Table className={classes.table} aria-label='simple table'>
+			<Table aria-label='simple table'>
 				<TableHead>
 					<TableRow>
 						<TableCell align='right'>Title</TableCell>
@@ -28,28 +20,13 @@ export default function ShoppingList({ fetchedProduct }) {
 				</TableHead>
 				<TableBody>
 					{fetchedProduct.map((product) => (
-						// console.log("product", product.category.image)
 						<TableRow key={product.id}>
-							<TableCell align='right'>
-								{product?.title}
-							</TableCell>
-							<TableCell align='right'>
-								{product?.price}
-							</TableCell>
-							<TableCell align='right'>
-								{product?.description}
-							</TableCell>
-							{product.category.image.length > 0 ? (
+							<TableCell align='right'>{product?.title}</TableCell>
+							<TableCell align='right'>{product?.price}</TableCell>
+							<TableCell align='right'>{product?.description}</TableCell>
 								<TableCell align='right'>
-									<img
-										src={product.category.image}
-										alt={product?.category.name}
-										style={{ width: "50%" }}
-									/>
+									<img src={product.category.image} alt={product?.category.name} style={{ width: "50%" }}/>
 								</TableCell>
-							) : (
-								<p>No Image to Display</p>
-							)}
 						</TableRow>
 					))}
 				</TableBody>
